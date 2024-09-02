d2e75c10998a0ac53af4e0a236f0d99d2490bf1c
Rizz-33 <risiniamarathunga@gmail.com>
2024-09-02 10:18:33 +0530

    chore: Identification of webelements

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/testcases/**/*.cy.{js,ts}",
+    //specPattern: "cypress/testcases/**/*.cy.{js,ts}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,5 +1,7 @@
 describe('Launch Application', () => {
     it('Launch the test application', () => {
-        cy.visit('https://www.saucedemo.com/v1/')
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+        cy.get("input[name='username']").type('rizz')
+        cy.get("input[name='password']").type('rizz')
     })
 })
