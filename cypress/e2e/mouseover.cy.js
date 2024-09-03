5fa41abbf47324e389e8dc41445fb29c9cca4eb6
Rizz-33 <risiniamarathunga@gmail.com>
2024-09-03 11:01:32 +0530

    test: Cypress test for mouseover functionality

---
+++ cypress/e2e/mouseover.cy.js
@@ -0,0 +1,7 @@
+describe('Select file Func', () => {
+    it('Select file', () => {
+        cy.visit('https://www.zoho.com/books/accounting-software-demo/#/timesheet/alltimeentries?filter_by=Status.All%2CDate.All&per_page=50')
+        cy.get('#ember302 > .list-actions > .btn-toolbar').trigger('mouseover')
+        cy.get('.list-actions > .btn-toolbar > :nth-child(3)').click()
+    })
+})
